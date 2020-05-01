5da2617193675e8f1dc113f89b9fb72e7617436d
Xavier Navarro <xanasa14@hotmail.com>
2020-05-01 11:13:13 -0500

    setup py

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='InvertedIndexFreq',  
+     name='InvertedIndexFreq',
      version='0.3',
-     scripts=['InvertedIndexFreq.py'] ,
+     py_modules = ["InvertedIndexFreq"] ,
+     packages_dir = ['','src'],
      author="Xavier Navarro",
      author_email="xanasa14@hotmail.com",
      description="Inverted Index Frequency Script",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
      url="https://github.com/xanasa14/InvertedIndexFreq_pkg",
      packages=setuptools.find_packages(),
      classifiers=[
