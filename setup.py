deefe241661e6d4f584c7264b1f5b161c25e2874
Xavier Navarro <xanasa14@hotmail.com>
2020-05-07 20:58:13 -0500

    set new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='InvertedIndexFreq',
-     version='0.6',
+     version='0.7',
      py_modules = ["InvertedIndexFreq"] ,
      package_dir={'':"src"},
      author="Xavier Navarro",
